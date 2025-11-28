ae802df6d6bbb697bb9b500e3152bd5a45cf3323
Guillaume DUHR <guillaume.duhr@gmail.com>
2025-11-28 09:59:33 +0100

    chore: update config sonar after typesript migration

--- api/web/src/setupTests.ts
+++ api/web/src/setupTests.ts
@@ -2,4 +2,15 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { expect, afterEach } from "vitest";
+import { cleanup } from "@testing-library/react";
+import * as matchers from "@testing-library/jest-dom/matchers";
+
+// extends Vitest's expect method with methods from react-testing-library
+expect.extend(matchers);
+
+// runs a cleanup after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  cleanup();
+});
